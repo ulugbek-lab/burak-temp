4c5339709a4ed047e415c18c61f308196c10b8c3
ulugbek-lab <bekievbek02@gmail.com>
2026-06-25 17:17:37 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
 function App() {
-  return <div>HOME</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Create React app on Typscript redux
+          </Typography>
+        </Box>
+        <Button variant="contained">contained</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import reportWebVitals from "./reportWebVitals";
+import theme from "./theme";
 import "./css/index.css";
-
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
