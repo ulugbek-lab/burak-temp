9ec8ff0b17bc0ba3736f97ad215e12802d7c0f41
ulugbek-lab <bekievbek02@gmail.com>
2026-06-27 13:20:38 +0900

    fix: develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,10 @@ import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch } from "react-router-dom";
-import { About } from "./screens/About";
-import { Users } from "./screens/Users";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/ProductsPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { UserPage } from "./screens/user.page";
 
 function App() {
   return (
@@ -12,34 +14,36 @@ function App() {
       <nav>
         <ul>
           <li>
+            <Link to="/products">ProductsPage</Link>
+          </li>
+          <li>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
+          </li>
+          <li>
             <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
           </li>
         </ul>
       </nav>
 
       <Switch>
-        <Route path="/about">
-          <About />
+        <Route path="/products">
+          <ProductsPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
         </Route>
         <Route path="/">
-          <Home />
+          <HomePage />
         </Route>
       </Switch>
     </div>
   );
-}
-
-function Home() {
-  return <Container>Home</Container>;
 }
 
 export default App;
