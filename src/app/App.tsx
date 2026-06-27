ccd7dc6846a0882cbdc57403855c9266276677c8
ulugbek-lab <bekievbek02@gmail.com>
2026-06-27 17:19:56 +0900

    feat: develop header and footers part 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/app.css";
+
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
@@ -10,6 +10,9 @@ import { UserPage } from "./screens/user.page";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import "../css/navbar.css";
+import { HelpPage } from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
@@ -27,6 +30,9 @@ function App() {
         </Route>
         <Route path="/member-page">
           <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
         </Route>
         <Route path="/">
           <HomePage />
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,3 +1,74 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function HomeNavbar() {
-    return <div>Home Navbar</div>
+  const authMember = null;
+  return (
+    <div className="home-navbar">
+      <Container sx={{ mt: "55px", height: "642px" }}>
+        <Stack
+          sx={{ height: "50px" }}
+          direction={"row"}
+          justifyContent={"space-between"}
+          align-items={"center"}
+        >
+          <Box>
+            <NavLink to={"/"}>
+              <img
+                style={{ width: "125px", height: "30px" }}
+                src="/icons/burak.svg"
+              />
+            </NavLink>
+          </Box>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            minWidth={"700"}
+            alignItems={"center"}
+          >
+            <Box className={"hover-line"}>
+              <NavLink to="/" activeClassName={"underline"}>
+                Home
+              </NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName={"underline"}>
+                products
+              </NavLink>
+            </Box>
+
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            {authMember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  MyPage
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            {/**Basket */}
+
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" style={{background:"#3776CC", color:"#f8f8ff"}}>Login</Button>
+              </Box>
+            ) : (
+              <img />
+            )}
+          </Stack>
+        </Stack>
+        <Stack>detail</Stack>
+      </Container>
+    </div>
+  );
 }
