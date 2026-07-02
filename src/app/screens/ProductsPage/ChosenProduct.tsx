7bcb04589f173d9deb44ff7fdba90e16039661b8
ulugbek-lab <bekievbek02@gmail.com>
2026-07-02 15:44:31 +0900

    feat: develop productPage screen component

---
+++ src/app/screens/ProductsPage/ChosenProduct.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Stack, Box } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import Divider from "../../components/divider";
+import Button from "@mui/material/Button";
+import Rating from "@mui/material/Rating";
+import { useParams } from "react-router-dom"; // @ts-ignore
+import { FreeMode, Navigation, Thumbs } from "swiper";
+// import "swiper/css";
+// import "swiper/css/free-mode";
+// import "swiper/css/navigation";
+// import "swiper/css/thumbs";
+
+export default function ChosenProduct() {
+  return (
+    <div className={"chosen-product"}>
+      <Box className={"title"}>Product Detail</Box>
+      <Container className={"product-container"}>
+        <Stack className={"chosen-product-slider"}>
+          <Swiper
+            loop={true}
+            spaceBetween={10}
+            navigation={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="swiper-area"
+          >
+            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+              (ele: string, index: number) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <img className="slider-image" src={ele} />
+                  </SwiperSlide>
+                );
+              },
+            )}
+          </Swiper>
+        </Stack>
+        <Stack className={"chosen-product-info"}>
+          <Box className={"info-box"}>
+            <strong className={"product-name"}>Kebab</strong>
+            <span className={"resto-name"}>Burak</span>
+            <span className={"resto-name"}>010-2469-4424</span>
+            <Box className={"rating-box"}>
+              <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+              <div className={"evaluation-box"}>
+                <div className={"product-view"}>
+                  <RemoveRedEyeIcon sx={{ mr: "10px" }} />
+                  <span>20</span>
+                </div>
+              </div>
+            </Box>
+            <p className={"product-desc"}>No Description</p>
+            <Divider height="1" width="100%" bg="#000000" />
+            <div className={"product-price"}>
+              <span>Price:</span>
+              <span>$12</span>
+            </div>
+            <div className={"button-box"}>
+              <Button variant="contained">Add To Basket</Button>
+            </div>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
--- src/app/screens/ProductsPage/index.tsx
+++ src/app/screens/ProductsPage/index.tsx
@@ -1,5 +1,22 @@
-import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
+import "../../../css/products.css";
 
 export default function ProductsPage() {
-  return <Container>ProductsPage</Container>;
+  const products = useRouteMatch();
+  console.log("products:", products);
+
+  return (
+    <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
