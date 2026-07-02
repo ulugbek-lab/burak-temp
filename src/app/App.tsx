3f8f0a6d8ce6e6da3cd95924522d023ca69df71e
ulugbek-lab <bekievbek02@gmail.com>
2026-07-02 17:21:27 +0900

    feat: publish helpPage and userPage screen components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import { Link, Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import ProductsPage from "./screens/productsPage";
 import OrdersPage from "./screens/ordersPage";
-import UserPage from "./screens/user.page";
+import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
--- src/app/screens/user.page/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Container } from "@mui/material";
-
-export default function UserPage() {
-  return <Container>UserPage</Container>;
-}
