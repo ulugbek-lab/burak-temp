7bbb034a2b8a5b3131af0c3b17fc9bc4bd5ab95d
ulugbek-lab <bekievbek02@gmail.com>
2026-07-28 18:02:51 +0900

    feat: publish auth model

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,16 +12,24 @@ import Footer from "./components/footer";
 import HelpPage from "./screens/helpPage";
 import Test from "./screens/Test";
 import { CartItem } from "../lib/types/search";
+import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
 
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import useBasket from "./hooks/useBasket";
 
 function App() {
   const location = useLocation();
 
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /** HANDLERS  */
+
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -60,6 +68,12 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
+      />
     </>
   );
 }
