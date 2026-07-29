30fc3ec87107a25c57fda768fdbece1c6d87cbfa
ulugbek-lab <bekievbek02@gmail.com>
2026-07-29 14:02:51 +0900

    feat: create login process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -8,7 +8,7 @@ import styled from "styled-components";
 import LoginIcon from "@mui/icons-material/Login";
 import { T } from "../../../lib/types/common";
 import { Messages } from "../../../lib/config";
-import { MemberInput } from "../../../lib/types/member";
+import { LoginInput, MemberInput } from "../../../lib/types/member";
 import MemberService from "../../services/MemberService";
 import { sweetErrorHandling } from "../../../lib/sweetAlert";
 
@@ -52,7 +52,6 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
   /** HANDLERS **/
 
   const handleUserName = (e: T) => {
-    console.log(e.target.value);
     setmemberNick(e.target.value);
   };
   const handlePhone = (e: T) => {
@@ -64,6 +63,8 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
   const handlePasswordKeyDown = (e: T) => {
     if (e.key === "Enter" && signupOpen) {
       handleSignupRequest().then();
+    } else if (e.key === "Enter" && signupOpen) {
+      handleLoginRequest().then();
     }
   };
 
@@ -74,19 +75,40 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
         memberNick !== "" && memberPhone !== "" && memberPassword !== "";
       if (!isFullfill) throw new Error(Messages.error3);
 
-      const signUpInput: MemberInput = {
+      const signupInput: MemberInput = {
         memberNick: memberNick,
         memberPhone: memberPhone,
         memberPassword: memberPassword,
       };
 
       const member = new MemberService();
-      const result = await member.signup(signUpInput);
+      const result = await member.signup(signupInput);
 
+      // Saving Authed user 
       handleSignupClose();
     } catch (err) {
       console.log(err);
       handleSignupClose();
+      sweetErrorHandling(err).then();
+    }
+  };
+  const handleLoginRequest = async () => {
+    try {
+      const isFullfill = memberNick !== "" && memberPassword !== "";
+      if (!isFullfill) throw new Error(Messages.error3);
+
+      const loginInput: LoginInput = {
+        memberNick: memberNick,
+        memberPassword: memberPassword,
+      };
+
+      const member = new MemberService();
+      const result = await member.login(loginInput);
+
+      handleLoginClose();
+    } catch (err) {
+      console.log(err);
+      handleLoginClose();
       sweetErrorHandling(err).then();
     }
   };
@@ -182,17 +204,21 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
                 label="username"
                 variant="outlined"
                 sx={{ my: "10px" }}
+                onChange={handleUserName}
               />
               <TextField
                 id={"outlined-basic"}
                 label={"password"}
                 variant={"outlined"}
                 type={"password"}
+                onChange={handlePassword}
+                onKeyDown={handlePasswordKeyDown}
               />
               <Fab
                 sx={{ marginTop: "27px", width: "120px" }}
                 variant={"extended"}
                 color={"primary"}
+                onClick={handleLoginRequest}
               >
                 <LoginIcon sx={{ mr: 1 }} />
                 Login
--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
 import { Product, ProductInquiry } from "../../lib/types/product";
-import { Member, MemberInput } from "../../lib/types/member";
+import { LoginInput, Member, MemberInput } from "../../lib/types/member";
 
 class MemberService {
   private readonly path: string;
@@ -38,7 +38,7 @@ class MemberService {
     try {
       const url = this.path + "/member/signup";
       const result = await axios.post(url, input, { withCredentials: true });
-      console.log(result);
+      console.log("signup",result);
 
       const member: Member = result.data.member;
       console.log(member);
@@ -47,6 +47,22 @@ class MemberService {
       return member;
     } catch (err) {
       console.log("error in signup", err);
+      throw err;
+    }
+  }
+  public async login(input: LoginInput): Promise<Member> {
+    try {
+      const url = this.path + "/member/login";
+      const result = await axios.post(url, input, { withCredentials: true });
+      console.log("login", result);
+
+      const member: Member = result.data.member;
+      console.log(member);
+      localStorage.setItem("memberData", JSON.stringify(member));
+
+      return member;
+    } catch (err) {
+      console.log("error login:", err);
       throw err;
     }
   }
